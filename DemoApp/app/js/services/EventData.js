707857df911d4999ba0e02e5c753b9b1503d7554
Dan Davis <daniel.davis@nih.gov>
2017-02-10 16:11:23 -0500

    Changes to node files do not work at the office.
    - Maybe they will work at home.

--- DemoApp/app/js/services/EventData.js
+++ DemoApp/app/js/services/EventData.js
@@ -1,42 +1,15 @@
 
-eventsApp.factory('eventData', function () {
+eventsApp.factory('eventData', function ($http, $log) {
   return {
-      event: {
-        name: 'Angular Boot Camp',
-        date: '1/1/2013',
-        time: '10:30 am',
-        location: {
-          address: 'Google Headquarters',
-          city: 'Mountain View',
-          province: 'CA'
-        },
-        imageUrl: '/img/angularjs-logo.png',
-        sessions: [
-          {
-            name: 'Directives Masterclass',
-            creatorName: 'Bob Smith',
-            duration: 1,
-            level: 'Advanced',
-            abstract: 'In this session you will learn..., but you should have a strong grasp of the introductory material',
-            upVoteCount: 0,
-          },
-          {
-            name: 'Scopes for fun and profit',
-            creatorName: 'John Doe',
-            duration: 2,
-            level: 'Introductory',
-            abstract: 'This session will take a closer look at ...',
-            upVoteCount: 0,
-          },
-          {
-            name: 'Well-behaved controllers',
-            creatorName: 'Jane Wise',
-            duration: 4,
-            level: 'Intermediate',
-            abstract: 'Controllers are the beginning of ...',
-            upVoteCount: 0,
-          }
-        ]
+    getEvent: function(successcb) {
+      $http({ method: 'GET', url: '/data/event/1' })
+        .success(function(data, status, headers, config) {
+          successcb(data);
+        })
+        .error(function(data, status, headers, config) {
+          $log.warn(data, status, headers, config);
+        });
       }
     };
-});
+  }
+);
