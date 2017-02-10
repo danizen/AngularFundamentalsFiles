15c92e5e427b182ea2033f34f43f8fc042138bae
Daniel Davis <dan@danizen.net>
2017-02-09 20:28:12 -0500

    Add sessions and first event responses.

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -11,7 +11,43 @@ eventsApp.controller('EventController',
         city: 'Mountain View',
         province: 'CA'
       },
-      imageUrl: '/img/angularjs-logo.png'
+      imageUrl: '/img/angularjs-logo.png',
+      sessions: [
+        {
+          name: 'Directives Masterclass',
+          creatorName: 'Bob Smith',
+          duration: '1 hour',
+          level: 'Advanced',
+          abstract: 'In this session you will learn...',
+          upVoteCount: 0,
+        },
+        {
+          name: 'Scopes for fun and profit',
+          creatorName: 'John Doe',
+          duration: '30 minutes',
+          level: 'Introductory',
+          abstract: 'This session will take a closer look at ...',
+          upVoteCount: 0,
+        },
+        {
+          name: 'Well-behaved controllers',
+          creatorName: 'Jane Wise',
+          duration: '2 hours',
+          level: 'Intermediate',
+          abstract: 'Controllers are the beginning of ...',
+          upVoteCount: 0,
+        }
+      ]
+    };
+
+    $scope.upVoteSession = function(session) {
+      session.upVoteCount++;
+    };
+
+    $scope.downVoteSession = function(session) {
+      if (session.upVoteCount > 0) {
+        session.upVoteCount--;
+      }
     };
   }
 );
